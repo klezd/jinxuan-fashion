6ceae1a60689d4fbd889be09e23fdde16054f260
siennad <siennad97@gmail.com>
2021-11-21 18:32:30 +0200

    Add not found page

--- src/page/NotFound/NotFound.jsx
+++ src/page/NotFound/NotFound.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-function NotFound(props) {
-	// const navigate = useNavigate();
-	// const dispatch = useDispatch();
-
-	// React.useEffect(() => {
-	// 	dispatch(getUser());
-	// }, []);
-
-	return <div className={styles.root}>Page Not Found</div>;
+function NotFound() {
+	return (
+		<div className={styles.root}>
+			<img src="/404-not-found.png"></img>
+		</div>
+	);
 }
 
 export default NotFound;
